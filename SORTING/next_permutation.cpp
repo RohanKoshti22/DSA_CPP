b6d140c697167efc92c89b1cdfcd30753789360b
Rohan Koshti <rohankoshti39@gmail.com>
2026-06-27 13:29:21 +0530

    DSA

---
+++ SORTING/next_permutation.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void next_permu(vector<int> &A){
+    int pivot =  -1;
+    int n = A.size();
+    for (int i = n-2; i >= 0 ; i--)
+    {
+        if(A[i] < A[i+1]){
+            pivot = i;
+            break;
+        }
+    }
+    if(pivot == -1 ){
+        reverse(A.begin(),A.end());
+        return;
+    }
+    
+    for (int i = n-1; i > pivot; i--)
+    {
+        if(A[i] > A[pivot]){
+            swap(A[i], A[pivot]);
+            break;
+        }
+    }
+    reverse(A.begin() + pivot + 1, A.end());
+}
+int main(){
+vector<int> A = {1,2,7,4,3,1};
+
+next_permu(A);
+    for(int i = 0; i < A.size(); i++){
+        cout << A[i] << " ";
+    }
+
+
+    return 0;
+}
