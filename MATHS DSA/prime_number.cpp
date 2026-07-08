4667a81320c380d3733a9516fa3e432c7a37650c
Rohan Koshti <rohankoshti39@gmail.com>
2026-07-08 14:15:24 +0530

    DSA

---
+++ MATHS DSA/prime_number.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int countPrimes(int n)
+{
+    vector<bool> isPrime(n + 1, true);
+    int count = 0;
+
+    for (int i = 2; i < n; i++)
+    {
+        if (isPrime[i])
+        {
+            count++;
+
+            for (int j = i * 2; j < n; j = j + i)
+            {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return count;
+}
+int main()
+{
+
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << countPrimes(n);
+
+    return 0;
+}
