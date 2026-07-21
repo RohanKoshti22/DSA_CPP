403f57f8369e87dd29803043bd17677da94c5280
Rohan Koshti <rohankoshti39@gmail.com>
2026-07-21 21:16:05 +0530

    DSA

---
+++ ARRAY SUM/duplicate_sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int>& nums) {
+
+    int slow = 0;
+    int fast = 0;
+
+    // Phase 1: Find the meeting point
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    // Phase 2: Find the duplicate number
+    slow = 0;
+
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
+
+int main() {
+
+    vector<int> nums = {1, 3, 4, 2, 2};
+
+    cout << "Duplicate Number: " << findDuplicate(nums);
+
+    return 0;
+}
