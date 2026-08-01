306004b52ac156a28a3c14cec6c249627d89ed8b
Rohan Koshti <rohankoshti39@gmail.com>
2026-08-01 14:18:02 +0530

    DSA

---
+++ RECURSION/sudoku_solver.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(vector<vector<char>> &board, int row, int col, char dig)
+{
+    // Check Row
+    for (int j = 0; j < 9; j++)
+    {
+        if (board[row][j] == dig)
+            return false;
+    }
+
+    // Check Column
+    for (int i = 0; i < 9; i++)
+    {
+        if (board[i][col] == dig)
+            return false;
+    }
+
+    // Check 3x3 Box
+    int startRow = (row / 3) * 3;
+    int startCol = (col / 3) * 3;
+
+    for (int i = startRow; i < startRow + 3; i++)
+    {
+        for (int j = startCol; j < startCol + 3; j++)
+        {
+            if (board[i][j] == dig)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool helper(vector<vector<char>> &board, int row, int col)
+{
+    // Base Case
+    if (row == 9)
+    {
+        return true;
+    }
+
+    int nextRow = row;
+    int nextCol = col + 1;
+
+    if (nextCol == 9)
+    {
+        nextRow = row + 1;
+        nextCol = 0;
+    }
+
+    // Already Filled
+    if (board[row][col] != '.')
+    {
+        return helper(board, nextRow, nextCol);
+    }
+
+    // Try digits 1 to 9
+    for (char dig = '1'; dig <= '9'; dig++)
+    {
+        if (isSafe(board, row, col, dig))
+        {
+            board[row][col] = dig;
+
+            if (helper(board, nextRow, nextCol))
+            {
+                return true;
+            }
+
+            // Backtrack
+            board[row][col] = '.';
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    vector<vector<char>> board =
+    {
+        {'5','3','.','.','7','.','.','.','.'},
+        {'6','.','.','1','9','5','.','.','.'},
+        {'.','9','8','.','.','.','.','6','.'},
+        {'8','.','.','.','6','.','.','.','3'},
+        {'4','.','.','8','.','3','.','.','1'},
+        {'7','.','.','.','2','.','.','.','6'},
+        {'.','6','.','.','.','.','2','8','.'},
+        {'.','.','.','4','1','9','.','.','5'},
+        {'.','.','.','.','8','.','.','7','9'}
+    };
+
+    helper(board, 0, 0);
+
+    cout << "Solved Sudoku:\n\n";
+
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
