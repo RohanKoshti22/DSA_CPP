bd7f7ebefffcb01b81be07a35686bdc47204b58c
Rohan Koshti <rohankoshti39@gmail.com>
2026-08-11 20:04:06 +0530

    DSA

---
+++ LINKED LIST/ll.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = NULL;
+            return;
+        }
+    }
+};
+int main()
+{
+
+    return 0;
+}
